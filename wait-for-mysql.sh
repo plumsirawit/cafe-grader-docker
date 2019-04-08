35d83342441b3d4e9bfff2ce4f38d9cbd9f88cb8
Sirawit Pongnakintr <s6007589@mwit.ac.th>
2019-04-08 06:05:53 +0000

    Added SSL tracking (Attempt)

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -13,4 +13,8 @@ if [ -f "/cafe_grader/setup.sh" ]; then
   /bin/bash /cafe_grader/setup.sh ;
 fi
 rm tmp/pids/server.pid
-rails s -p 3000 -b '0.0.0.0'
+# For non-SSL purpose
+# rails s -p 3000 -b '0.0.0.0'
+
+# with-ssl-cert (Sirawit, 8/4/2019)
+thin -a 0.0.0.0 -p 3000 --ssl --ssl-verify --ssl-key-file /cafe_grader/server.key --ssl-cert-file /cafe_grader/server.crt start
